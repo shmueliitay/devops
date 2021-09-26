5808e6b118e115927856379e2390691b8d38b437
Itay Shmueli <shmueliitay@gmail.com>
2021-09-26 14:38:53 +0300

    change java permission in test code22

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,7 +38,7 @@ public class TestGreeter {
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
-	--permit-illegal-access
+	
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
