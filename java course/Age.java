6f3127a589ca832ce3ebbaf80182dd2a9cc28395
itay shmueli <shmueliitay@gmail.com>
2025-01-12 12:01:59 +0200

    push test to itay github

---
+++ java course/Age.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Age {
+    public static void main(String[] args) {  
+        Scanner scanner = new scanner(System.in);
+        byte age = scanner.nextByte();
+        System.out.println("You are " + age);  
+    }
+}
